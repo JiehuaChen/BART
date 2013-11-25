1454d6e16dda9afc6aba6844eb8b02443b70d9b0
Zhang Zhiming <smartzzm@gmail.com>
2013-11-25 22:10:01 +0800

    [spLM] add compilation support for spLM.cpp
    and change string to std:string

--- src/spLM.cpp
+++ src/spLM.cpp
@@ -10,10 +10,10 @@
 extern "C" {
 
 double** spLM(double* Y_r, double* X_r, int p_r, int n_r, double* coordsD_r,
-	    string betaPrior_r, double** betaNorm_r, double* sigmaSqIG_r, double* tauSqIG_r, double* nuUnif_r, double* phiUnif_r,
+	    std::string betaPrior_r, double** betaNorm_r, double* sigmaSqIG_r, double* tauSqIG_r, double* nuUnif_r, double* phiUnif_r,
 	    double phiStarting_r, double sigmaSqStarting_r, double tauSqStarting_r, double nuStarting_r,
 	    double phiTuning_r, double sigmaSqTuning_r, double tauSqTuning_r, double nuTuning_r, 
-	    bool nugget_r, string covModel_r, bool amcmc_r, int nBatch_r, int batchLength_r, double acceptRate_r, int verbose_r, int nReport_r){
+	    bool nugget_r, std::string covModel_r, bool amcmc_r, int nBatch_r, int batchLength_r, double acceptRate_r, int verbose_r, int nReport_r){
 
     /*****************************************
                 Common variables
