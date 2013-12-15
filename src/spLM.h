8dad7b5c0b2dca4b18fc86c545adc9ffa463cca1
Zhang Zhiming <smartzzm@gmail.com>
2013-12-15 22:25:12 +0800

    [spLM] remove 'extern' from the definition of spLM function

--- src/spLM.h
+++ src/spLM.h
@@ -6,7 +6,7 @@ double** spLM(double* Y_r, int n_r, double* coordsD_r,
 	    double* phiUnif_r, double phiStarting_r, double sigmaSqStarting_r, 
 	    double tauSqStarting_r, double nuStarting_r,double phiTuning_r, 
 	    double sigmaSqTuning_r, double tauSqTuning_r, double nuTuning_r, 
-	    bool nugget_r, string covModel_r, bool amcmc_r, 
+	    bool nugget_r, std::string covModel_r, bool amcmc_r, 
 	    int nBatch_r, int batchLength_r, double acceptRate_r, 
 	    int verbose_r, int nReport_r);
 
